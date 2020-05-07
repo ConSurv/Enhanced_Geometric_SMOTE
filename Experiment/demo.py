de27b51e690b81113e4320f9018629c6c77e44f6
Vivek Vinushanth <cvivekvinushanth@gmail.com>
2020-05-07 12:26:22 +0530

    visualizer refactored.

--- Experiment/demo.py
+++ Experiment/demo.py
@@ -5,12 +5,14 @@ import pandas as pd
 from Experiment.visualizer import visualize
 from egsmote import EGSmote
 from collections import Counter
+import numpy as np
+
 
 # Read CSV file
 df = pd.read_csv('../Experiment/data/NSLKDD-mini.csv')
 
-X = df.iloc[:, :-1].values
-y = df.iloc[:, -1].values
+X = np.asarray(df.iloc[:, :-1].values)
+y = np.asarray(df.iloc[:, -1].values)
 
 print('Original dataset shape %s' % (Counter(y)))
 # Original dataset shape Counter({{1: 28416, 0: 150}})
--- Experiment/visualizer.py
+++ Experiment/visualizer.py
@@ -4,9 +4,6 @@ import datetime
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
 
-
-# Partition the dataset
-
 def visualize(X, y, X_resampled, y_resampled):
     # Instantiate a PCA object for the sake of easy visualisation
     pca = PCA(n_components=2)
@@ -20,15 +17,15 @@ def visualize(X, y, X_resampled, y_resampled):
     # Two subplots, unpack the axes array immediately
     f, (ax1, ax2) = plt.subplots(1, 2)
 
-    c0 = ax1.scatter(X_vis[y == '0', 0], X_vis[y == '0', 1], label="Class #0", alpha=0.5, marker='.')
-    c1 = ax1.scatter(X_vis[y == '1', 0], X_vis[y == '1', 1], label="Class #1", alpha=0.5, marker='.')
-    ax1.set_title('Original Data')
+    c0 = ax1.scatter(X_vis[y == 0, 0], X_vis[y == 0, 1], label="Class #0", color='orange', alpha=0.5, marker='.')
+    c1 = ax1.scatter(X_vis[y == 1, 0], X_vis[y == 1, 1], label="Class #1", color='blue', alpha=0.5, marker='.')
+    ax1.set_title('Before ReSampling')
 
-    ax2.scatter(X_res_vis[y_resampled == '0', 0], X_res_vis[y_resampled == '0', 1],
-                label="Class #0", alpha=0.5, marker='.')
-    ax2.scatter(X_res_vis[y_resampled == '1', 0], X_res_vis[y_resampled == '1', 1],
-                label="Class #1", alpha=0.5, marker='.')
-    ax2.set_title('After EG-SMOTE ReSampling')
+    ax2.scatter(X_res_vis[y_resampled == 0, 0], X_res_vis[y_resampled == 0, 1],
+                label="Class #0", alpha=0.5, marker='.', color='orange')
+    ax2.scatter(X_res_vis[y_resampled == 1, 0], X_res_vis[y_resampled == 1, 1],
+                label="Class #1", alpha=0.5, marker='.', color='blue')
+    ax2.set_title('After EG-SMOTE')
 
     # make nice plotting
     for ax in (ax1, ax2):
@@ -38,10 +35,8 @@ def visualize(X, y, X_resampled, y_resampled):
         ax.get_yaxis().tick_left()
         ax.spines['left'].set_position(('outward', 10))
         ax.spines['bottom'].set_position(('outward', 10))
-        ax.set_xlim([-6, 8])
-        ax.set_ylim([-6, 6])
 
-    f.legend((c0, c1), ('Class #0', 'Class #1'), loc='lower center', ncol=2, labelspacing=0.5)
+    f.legend((c0, c1), ('Class #0', 'Class #1'), loc='lower center', ncol=2)
     plt.tight_layout(pad=3)
-    # plt.show()
-    plt.savefig("output/oversampled_" + datetime.datetime.now().strftime("%Y-%m-%d__%H_%M_%S") + ".png", dpi=2000)
+    plt.show()
+    plt.savefig("output/re_sampled_" + datetime.datetime.now().strftime("%Y-%m-%d__%H_%M_%S") + ".png", dpi=2000)
